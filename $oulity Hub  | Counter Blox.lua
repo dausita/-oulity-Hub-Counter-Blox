ef7aac86d73a343d29c1c2eb7177b8668f0f7673
dausita <delikanyagiz.35@gmail.com>
2025-03-15 22:32:42 +0300

    Update $oulity Hub  | Counter Blox.lua

--- $oulity Hub  | Counter Blox.lua
+++ $oulity Hub  | Counter Blox.lua
@@ -1,3 +1,8 @@
+if game.PlaceId ~= 301549746 then
+    game.Players.LocalPlayer:Kick("Not Supported Game.")
+    return
+end
+
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
